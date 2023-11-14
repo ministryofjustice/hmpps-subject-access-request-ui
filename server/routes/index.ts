6199a34bcaa45e728254043c1a20f76cdb88b076
Anna Horstmann <annahorstmann@mt-mbp-kqgl49fmc2.home>
2023-11-14 09:23:15 -0800

    Add service selection page.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -12,5 +12,9 @@ export default function routes(service: Services): Router {
     res.render('pages/index')
   })
 
+  get('/serviceselection', (req, res, next) => {
+    res.render('pages/serviceselection')
+  })
+
   return router
 }
