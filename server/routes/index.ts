315f3258e1a6463641bcf98bca3ee5244200a599
Anna Horstmann <annahorstmann@mt-mbp-kqgl49fmc2.home>
2023-11-15 07:31:38 -0800

    Fix parameter passing to enable today's date.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,10 +2,12 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
+import { formatDate } from '../utils/dateHelpers'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(service: Services): Router {
   const router = Router()
+  const now = new Date()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res, next) => {
@@ -13,7 +15,10 @@ export default function routes(service: Services): Router {
   })
 
   get('/inputs', (req, res, next) => {
-    res.render('pages/inputs')
+    res.render('pages/inputs', {
+      today: formatDate(now.toISOString(), 'short'),
+      now: now.getHours().toString(),
+    })
   })
 
   return router
---
+++ server/utils/dateHelpers.ts
@@ -0,0 +1,160 @@
+import { add, addBusinessDays, addDays, addMonths, addWeeks, format, formatISO, isValid, parse } from 'date-fns'
+import logger from '../../logger'
+
+/**
+ * Format a Date object as an ISO-8601 string, rendering only the date part.
+ *
+ * e.g. 31/01/2023 -> 2023-01-31
+ *
+ * @param date
+ */
+export const formatDateISO = (date: Date): string => {
+  let dateStr
+  try {
+    dateStr = formatISO(date, { representation: 'date' })
+  } catch (error) {
+    logger.error(`Error: formatDateISO - ${error.message}`)
+  }
+  return dateStr
+}
+
+/**
+ * Format a Date object as an ISO-8601 string, optionally setting the time to 00:00 or 23:59
+ *
+ * @param date
+ * @param options
+ */
+export const formatDateTimeISO = (date: Date, options?: { startOfDay?: boolean; endOfDay?: boolean }): string => {
+  let dateStr
+  const adjustedDate = date
+  if (options?.startOfDay) {
+    adjustedDate.setHours(0)
+    adjustedDate.setMinutes(0)
+    adjustedDate.setSeconds(0)
+  } else if (options?.endOfDay) {
+    adjustedDate.setHours(23)
+    adjustedDate.setMinutes(59)
+    adjustedDate.setSeconds(59)
+  }
+  try {
+    dateStr = format(adjustedDate, "yyyy-MM-dd'T'HH:mm:ss")
+  } catch (error) {
+    logger.error(`Error: formatDateTimeISO - ${error.message}`)
+  }
+  return dateStr
+}
+
+/**
+ * Parse date string to a Date object
+ *
+ * Valid date strings are in day/month/year format, with either 1 or 2 digits for day and month, and 4 digits for year
+ *
+ * Separator can be any of `-/,. `
+ *
+ * @param date
+ */
+export const parseDate = (date: string): Date => {
+  const dateFormatPattern = /(\d{1,2})([-/,. ])(\d{1,2})[-/,. ](\d{4})/
+
+  if (!isRealDate(date)) return new Date(NaN)
+
+  const separator = date.match(dateFormatPattern)[2]
+  return parse(date, `dd${separator}MM${separator}yyyy`, new Date())
+}
+
+/**
+ * Return true is date string is a real date
+ *
+ * Checks if format is valid *and* the date is valid (e.g. not 30th February)
+ *
+ * @param date
+ */
+export const isRealDate = (date: string): boolean => {
+  const dateFormatPattern = /(\d{1,2})([-/,. ])(\d{1,2})[-/,. ](\d{4})/
+
+  if (!dateFormatPattern.test(date)) return false
+  const separator = date.match(dateFormatPattern)[2]
+  return isValid(parse(date, `dd${separator}MM${separator}yyyy`, new Date()))
+}
+
+/**
+ * Formats an ISO-8601 date string to standard gov.uk display format, e.g. 20 January 2023
+ *
+ * Also supports passing in an optional style string to output other standard formats:
+ * short, full and medium - e.g. '20/01/2023', 'Friday, 20 January 2023' and '20 Jan 2023'
+ *
+ * @param isoDate ISO-8601 format date string
+ * @param style formatting style to use - long (default), short, full, medium
+ * @returns formatted date string
+ */
+export const formatDate = (isoDate: string, style: 'short' | 'full' | 'long' | 'medium' = 'long'): string => {
+  if (!isoDate) return ''
+  return new Date(isoDate).toLocaleDateString('en-gb', { dateStyle: style })
+}
+
+/**
+ * Formats an ISO-8601 datetime string to a human readable string, e.g. 20 January 2023 at 16:27
+ *
+ * Also supports passing in an optional style string to output other standard formats:
+ * short, full and medium - e.g. '20/01/2023 16:27', 'Friday, 20 January 2023 at 16:27' and '20 Jan 2023 at 16:27'
+ *
+ * @param isoDate ISO-8601 format date string
+ * @param style formatting style to use - long (default), short, full, medium
+ * @param separator separator between date and time - default ' ' for short or ' at ' for others
+ * @returns formatted datetime string
+ */
+export const formatDateTime = (
+  isoDate: string,
+  style: 'short' | 'full' | 'long' | 'medium' = 'long',
+  separator: string = undefined,
+): string => {
+  if (!isoDate) return ''
+
+  let datetimeStr = new Date(isoDate).toLocaleDateString('en-gb', { dateStyle: style })
+  if (!separator) {
+    if (style === 'short') {
+      datetimeStr += ' '
+    } else {
+      datetimeStr += ' at '
+    }
+  } else {
+    datetimeStr += separator
+  }
+  datetimeStr += new Date(isoDate).toLocaleTimeString('en-gb', { hour: '2-digit', minute: '2-digit' })
+  return datetimeStr
+}
+
+export const timeFormat = (dateTimeStr: string) => {
+  return new Date(dateTimeStr).toLocaleTimeString('en-gb', { hour: '2-digit', minute: '2-digit' })
+}
+
+export const dateToIsoDate = (date: string) => {
+  return formatDateISO(parseDate(date))
+}
+
+export const formatDateWithTime = (date: string, hours: string, minutes: string) => {
+  return `${date}T${hours}:${minutes}`
+}
+
+export const calculateEndDate = (startDate: Date, repeatPeriod: string, times: number): Date => {
+  const adjustedTimes = times - 1 // Appointments include the initial one
+
+  if (adjustedTimes < 1) {
+    return startDate
+  }
+
+  switch (repeatPeriod) {
+    case 'DAILY':
+      return addDays(startDate, adjustedTimes)
+    case 'WEEKDAYS':
+      return addBusinessDays(startDate, adjustedTimes)
+    case 'WEEKLY':
+      return addWeeks(startDate, adjustedTimes)
+    case 'FORTNIGHTLY':
+      return add(startDate, { weeks: adjustedTimes * 2 })
+    case 'MONTHLY':
+      return addMonths(startDate, adjustedTimes)
+    default:
+      return null
+  }
+}
