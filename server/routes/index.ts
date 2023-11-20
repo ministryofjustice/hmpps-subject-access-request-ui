bce36aaeb6bc2119b32fcef9b280d163f514ee7a
Anna Horstmann <annahorstmann@mt-mbp-kqgl49fmc2.home>
2023-11-20 04:28:33 -0800

    Remove unused dateHelpers functions and add test for remaining.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,7 +2,7 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
-import { formatDate } from '../utils/dateHelpers'
+import formatDate from '../utils/dateHelpers'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(service: Services): Router {
