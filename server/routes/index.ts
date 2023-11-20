8ed1e012d57a0ea85136ca55cb340ad42a044fb6
Cai Kingston <cai.kingston@digital.justice.gov.uk>
2023-11-20 12:06:28 +0000

    fix linting

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,7 +7,6 @@ import { formatDate } from '../utils/dateHelpers'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(service: Services): Router {
   const router = Router()
-  const now = new Date()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
 
@@ -18,31 +17,23 @@ export default function routes(service: Services): Router {
   // --- option 1 ---
 
   get('/inputs', (req, res, next) => {
-    console.log('/INPUTS CALLED')
-    console.log(req.session)
     res.render('pages/inputs', {
-      today: formatDate(now.toISOString(), 'short'),
+      today: formatDate(new Date().toISOString(), 'short'),
     })
   })
 
   post('/get-input', (req, res, next) => {
-    console.log('/GET-INPUT CALLED')
-    console.log(req.session)
     req.session.userData = {
       dateFrom: req.body.dateFrom,
       dateTo: req.body.dateTo,
       caseReference: req.body.caseReference,
     }
-    // -- put inputs in session store
-    // -- redirect user to /service
     res.redirect('/services')
   })
 
   get('/services', (req, res, next) => {
-    console.log('/SERVICES CALLED')
-    console.log(req.session)
     res.render('pages/inputs', {
-      today: formatDate(now.toISOString(), 'short'),
+      today: formatDate(new Date().toISOString(), 'short'),
     })
   })
 
