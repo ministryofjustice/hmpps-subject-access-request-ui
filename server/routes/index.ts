c86c484de489c8ffa088cf1f5f284fa6beca2b46
Anna Horstmann <annahorstmann@mt-mbp-kqgl49fmc2.home>
2023-11-21 04:57:08 -0800

    Add service-catalogue to docker-compose for running locally.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,6 @@
 import { type RequestHandler, Router } from 'express'
 
+import axios from 'axios'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 
@@ -12,9 +13,27 @@ export default function routes(service: Services): Router {
     res.render('pages/index')
   })
 
+  /* call function callApi, return services list */
+  const url = ''
+  const servicelist = getServices(url)
+
   get('/serviceselection', (req, res, next) => {
-    res.render('pages/serviceselection')
+    res.render('pages/serviceselection', servicelist)
+    /* pass services list */
   })
 
   return router
+}
+
+/* function callApi:
+make get request to API
+return list of services */
+
+async function getServices(apiurl: string) {
+  try {
+    const response = await axios.get(apiurl)
+    // Handle the response
+  } catch (error) {
+    // Handle the error
+  }
 }
