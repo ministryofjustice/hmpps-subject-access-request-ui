17acaf22963283abdf5b3079eaf4e732d838ee33
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-11-22 12:09:26 +0000

    Add happy path integration tests for inputs page

--- integration_tests/e2e/inputs.cy.ts
+++ integration_tests/e2e/inputs.cy.ts
@@ -18,13 +18,52 @@ context('Inputs', () => {
     cy.request('/inputs').its('body').should('exist')
   })
 
-  it('Contains all necessary input components', () => {
+  it('Contains all necessary input components with default values', () => {
     cy.signIn()
     cy.visit('/inputs')
     const inputsPage = Page.verifyOnPage(InputsPage)
     inputsPage.datePickerFrom().should('exist')
+    inputsPage.datePickerFrom().should('have.value', '')
     inputsPage.datePickerTo().should('exist')
+    inputsPage.datePickerTo().should('have.value', new Date().toLocaleDateString('en-gb', { dateStyle: 'short' }))
     inputsPage.caseReferenceTextbox().should('exist')
+    inputsPage.caseReferenceTextbox().should('have.value', '')
     inputsPage.continueButton().should('exist')
+  })
+
+  it('Submits user inputs and redirects to /services', () => {
+    cy.intercept({
+      method: 'POST',
+      url: '/save-inputs',
+    }).as('saveInputs')
+    cy.signIn()
+    cy.visit('/inputs')
+    const inputsPage = Page.verifyOnPage(InputsPage)
+    inputsPage.datePickerFrom().clear().type('01/01/2001')
+    inputsPage.datePickerTo().clear().type('01/01/2021')
+    inputsPage.caseReferenceTextbox().clear().type('exampleCaseReference')
+    inputsPage.continueButton().click()
+    cy.wait('@saveInputs').then(interception => {
+      cy.wrap(interception.request.body)
+        .should('include', 'dateFrom=')
+        .and('include', 'dateTo=')
+        .and('include', 'caseReference=')
+    })
+    cy.url().should('to.match', /services$/)
+  })
+
+  it('Persists user inputs when returning to inputs page', () => {
+    cy.signIn()
+    cy.visit('/inputs')
+    let inputsPage = Page.verifyOnPage(InputsPage)
+    inputsPage.datePickerFrom().clear().type('01/01/2001')
+    inputsPage.datePickerTo().clear().type('01/01/2021')
+    inputsPage.caseReferenceTextbox().clear().type('exampleCaseReference')
+    inputsPage.continueButton().click()
+    cy.visit('/inputs')
+    inputsPage = Page.verifyOnPage(InputsPage)
+    inputsPage.datePickerFrom().should('have.value', '01/01/2001')
+    inputsPage.datePickerTo().should('have.value', '01/01/2021')
+    inputsPage.caseReferenceTextbox().should('have.value', 'exampleCaseReference')
   })
 })
