b4730af52f6527e3ca65e779d3ea299b599d8090
HorstmannA <anna.horstmann@madetech.com>
2023-11-23 06:23:33 -0800

    Add service catalogue url as environment variable and use 'Select All' button rather than checkbox.

--- server/data/serviceCatalogueClient.ts
+++ server/data/serviceCatalogueClient.ts
@@ -1,23 +1,16 @@
 import superagent from 'superagent'
+import config from '../config'
 
-const apiurl = 'http://localhost:1337/products'
 export default class ServiceCatalogueClient {
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   getServices(token: string): Promise<any[]> {
     return new Promise(resolve => {
       superagent
-        .get(apiurl)
+        .get(`${config.apis.serviceCatalogue.url}/products`)
         .set('Authorization', `OAuth ${token}`)
         .end((error, res) => {
           if (error) {
             const list = [
-              {
-                value: 'Select All',
-                text: 'Select All',
-              },
-              {
-                divider: 'or',
-              },
               { text: 'Data from TestService1', value: 'https://foo.boo.com' },
               { text: 'Data from TestService2', value: 'https://foo.boo.com' },
               { text: 'Data from TestService3', value: 'https://foo.boo.com' },
