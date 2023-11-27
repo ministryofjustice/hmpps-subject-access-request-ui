14849d083ebaa6029e5e064f579715e1f835ef91
Cai Kingston <cai.kingston@digital.justice.gov.uk>
2023-11-27 11:57:36 +0000

    fix linting

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,7 +3,6 @@ import ServiceCatalogueClient from '../data/serviceCatalogueClient'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import InputsController from '../controllers/inputsController'
-import formatDate from '../utils/dateHelpers'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(service: Services): Router {
@@ -11,14 +10,14 @@ export default function routes(service: Services): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
   const catalogueclient = new ServiceCatalogueClient()
-  
+
   get('/', (req, res, next) => {
     res.render('pages/index')
   })
 
   get('/inputs', InputsController.getInputs)
   post('/inputs', InputsController.saveInputs)
-  
+
   get('/serviceselection', (req, res, next) => {
     // TODO: GetServiceToken
     catalogueclient.getServices('mockToken').then(list => {
