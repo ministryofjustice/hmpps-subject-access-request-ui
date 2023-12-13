b766c673f828c0d6bfd81b74b9dfb36520068c56
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-12-13 14:18:35 +0000

    Add page elements

--- integration_tests/e2e/confirmation.cy.ts
+++ integration_tests/e2e/confirmation.cy.ts
@@ -1,5 +1,6 @@
 import Page from '../pages/page'
 import AuthSignInPage from '../pages/authSignIn'
+import ConfirmationPage from '../pages/confirmation'
 
 context('Confirmation', () => {
   beforeEach(() => {
@@ -8,7 +9,6 @@ context('Confirmation', () => {
     cy.task('stubManageUser')
   })
 
-  // All pages direct users to auth
   it('Redirects to auth if requested by unauthenticated user', () => {
     cy.visit('/confirmation')
     Page.verifyOnPage(AuthSignInPage)
@@ -17,5 +17,27 @@ context('Confirmation', () => {
   it('Renders for authenticated users', () => {
     cy.signIn()
     cy.visit('/confirmation')
+  })
+
+  it('Displays confirmation panel', () => {
+    cy.signIn()
+    cy.visit('/confirmation')
+    const confirmationPage = Page.verifyOnPage(ConfirmationPage)
+    confirmationPage.confirmationPanel().should('exist')
+  })
+
+  it('Displays next steps', () => {
+    cy.signIn()
+    cy.visit('/confirmation')
+    const confirmationPage = Page.verifyOnPage(ConfirmationPage)
+    confirmationPage.nextSteps().should('exist')
+  })
+
+  it('Displays link to view reports page', () => {
+    cy.signIn()
+    cy.visit('/confirmation')
+    const confirmationPage = Page.verifyOnPage(ConfirmationPage)
+    confirmationPage.viewReportsLink().should('exist')
+    confirmationPage.viewReportsLink().should('have.attr', 'href', '/reports')
   })
 })
