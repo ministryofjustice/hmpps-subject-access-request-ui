19808a4dcf7d4d607cf30cfcf4e1dceb3a1cae81
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-01-11 22:51:56 +0000

    Expand validation tests

--- server/controllers/subjectIdValidation.test.ts
+++ server/controllers/subjectIdValidation.test.ts
@@ -3,8 +3,9 @@ import SubjectIdValidation from './subjectIdValidation'
 describe('validateSubjectId', () => {
   const errors = {
     missing: 'Enter subject ID',
-    valid: 'Subject ID must be a NOMIS prisoner number or nDelius case reference number',
+    invalid: 'Subject ID must be a NOMIS prisoner number or nDelius case reference number',
   }
+  const accepted = ''
 
   test('validates that a subject ID is provided', () => {
     const subjectIdNotProvided = ''
@@ -12,13 +13,26 @@ describe('validateSubjectId', () => {
     expect(SubjectIdValidation.validateSubjectId(subjectIdNotProvided)).toEqual(errors.missing)
   })
 
-  test('validates that the subject ID is in a valid format for a NOMIS or nDelius ID', () => {
-    const exampleNomisId = 'A1234BC'
-    const exampleNdeliusId = 'A123456'
-    const invalidSubjectId = 'A1A'
+  describe('when a subject ID is provided', () => {
+    test('accepts subject IDs in valid NOMIS ID format', () => {
+      const validNomisIds = ['A1234BC', 'a1234bc', 'z9876YX', ' A1234BC', 'A1234BC ']
+      validNomisIds.forEach(validNomisId => {
+        expect(SubjectIdValidation.validateSubjectId(validNomisId)).toEqual(accepted)
+      })
+    })
 
-    expect(SubjectIdValidation.validateSubjectId(exampleNomisId)).toEqual('')
-    expect(SubjectIdValidation.validateSubjectId(exampleNdeliusId)).toEqual('')
-    expect(SubjectIdValidation.validateSubjectId(invalidSubjectId)).toEqual(errors.valid)
+    test('accepts subject IDs in valid nDelius ID format', () => {
+      const validNdeliusIds = ['A123456', 'a123456', ' Z098765', 'Z098765 ']
+      validNdeliusIds.forEach(validNdelousId => {
+        expect(SubjectIdValidation.validateSubjectId(validNdelousId)).toEqual(accepted)
+      })
+    })
+
+    test('rejects subject IDs in neither valid NOMIS or nDelius format', () => {
+      const invalidIds = ['A1234B', 'a1234bcd', 'z987a6Y', 'A123 4BC', '-A12345']
+      invalidIds.forEach(invalidId => {
+        expect(SubjectIdValidation.validateSubjectId(invalidId)).toEqual(errors.invalid)
+      })
+    })
   })
 })
--- server/controllers/subjectIdValidation.ts
+++ server/controllers/subjectIdValidation.ts
@@ -8,7 +8,7 @@ export default class SubjectIdValidation {
         throw new ValidationError('Enter subject ID')
       }
       // validate that format matches Nomis or nDelius ID format
-      if (!/^[A-Za-z][0-9]{4}[A-Za-z]{2}$/.exec(subjectId) && !/^[A-Za-z][0-9]{6}$/.exec(subjectId)) {
+      if (!/^[A-Za-z][0-9]{4}[A-Za-z]{2}$/.exec(subjectId.trim()) && !/^[A-Za-z][0-9]{6}$/.exec(subjectId.trim())) {
         throw new ValidationError('Subject ID must be a NOMIS prisoner number or nDelius case reference number')
       }
     } catch (e) {
