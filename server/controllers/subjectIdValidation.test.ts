b78d88fc5d07e64d38f81a8a7181a8c3def71b12
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-01-11 22:28:22 +0000

    Add validation against nomis and ndelius formats

--- server/controllers/subjectIdValidation.test.ts
+++ server/controllers/subjectIdValidation.test.ts
@@ -1,13 +1,24 @@
 import SubjectIdValidation from './subjectIdValidation'
 
 describe('validateSubjectId', () => {
-  const missing = 'Enter subject ID'
+  const errors = {
+    missing: 'Enter subject ID',
+    valid: 'Subject ID must be a NOMIS prisoner number or nDelius case reference number',
+  }
 
   test('validates that a subject ID is provided', () => {
-    const subjectIdProvided = 'ExampleSubjectId'
     const subjectIdNotProvided = ''
 
-    expect(SubjectIdValidation.validateSubjectId(subjectIdProvided)).toEqual('')
-    expect(SubjectIdValidation.validateSubjectId(subjectIdNotProvided)).toEqual(missing)
+    expect(SubjectIdValidation.validateSubjectId(subjectIdNotProvided)).toEqual(errors.missing)
+  })
+
+  test('validates that the subject ID is in a valid format for a NOMIS or nDelius ID', () => {
+    const exampleNomisId = 'A1234BC'
+    const exampleNdeliusId = 'A123456'
+    const invalidSubjectId = 'A1A'
+
+    expect(SubjectIdValidation.validateSubjectId(exampleNomisId)).toEqual('')
+    expect(SubjectIdValidation.validateSubjectId(exampleNdeliusId)).toEqual('')
+    expect(SubjectIdValidation.validateSubjectId(invalidSubjectId)).toEqual(errors.valid)
   })
 })
--- server/controllers/subjectIdValidation.ts
+++ server/controllers/subjectIdValidation.ts
@@ -7,6 +7,10 @@ export default class SubjectIdValidation {
       if (!subjectId) {
         throw new ValidationError('Enter subject ID')
       }
+      // validate that format matches Nomis or nDelius ID format
+      if (!/^[A-Za-z][0-9]{4}[A-Za-z]{2}$/.exec(subjectId) && !/^[A-Za-z][0-9]{6}$/.exec(subjectId)) {
+        throw new ValidationError('Subject ID must be a NOMIS prisoner number or nDelius case reference number')
+      }
     } catch (e) {
       return e.message
     }
