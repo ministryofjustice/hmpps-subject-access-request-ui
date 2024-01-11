81a9aee04eaa9e065926e40f33a060dd68fd064f
HorstmannA <anna.horstmann@madetech.com>
2024-01-11 12:29:17 +0000

    Remove comments.

--- server/controllers/summaryController.test.ts
+++ server/controllers/summaryController.test.ts
@@ -54,14 +54,6 @@ describe('getReportDetails', () => {
       body: { selectedservices: [] },
     }
 
-    // '{ "dateFrom": "01/12/2023",
-    // "dateTo": "03/01/2024",
-    // "sarCaseReferenceNumber": "1234abc",
-    // "services": "1,2,4",
-    // "nomisId": "",
-    // "ndeliusCaseReferenceId": "" }
-
-    // const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRwcy1jbGllbnQta2V5In0.eyJzdWIiOiJwcmlzb25lci1vZmZlbmRlci1zZWFyY2gtY2xpZW50IiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJhdXRoX3NvdXJjZSI6Im5vbmUiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwOTAvYXV0aC9pc3N1ZXIiLCJleHAiOjE3MDQ4ODcyNTIsImF1dGhvcml0aWVzIjpbIlJPTEVfU1lTVEVNX1VTRVIiLCJST0xFX1BSSVNPTkVSX0lOREVYIiwiUk9MRV9HTE9CQUxfU0VBUkNIIl0sImp0aSI6Ilhvbk1QMjhGTzUwNUtOQzMzN3RfdWlhMDkzVSIsImNsaWVudF9pZCI6InByaXNvbmVyLW9mZmVuZGVyLXNlYXJjaC1jbGllbnQifQ.n5YaTz5W8-RhhfZof7gMLqM644ifAWC0e96EJ6IDM17iosX_UF4OHAFuC2Puw7UEYPYcmVMedBlTLLJrCLG7fN4wfTSldhQOLYO-aDl4nT5JKzptTkWV1KrJsAA9TZO4VIy3DJyYfPluMWq6mf5rjRiy_ud5rsUh5_lXGQPwq532yUXqPbgxDdHDMLVVwj6WNUQUXlpBub7qXXMIRYE-v9d27_kRxnhb7sJdBCk8Fpyx4duNozU9bQCX7Vq1YPqCo-_tFP6Gnoc7rJu24Y3UnCJGjJiAorN_Mgickd5qhXcboJa1TT8nFgN8QWNYG4R9AauUXKfkF43pnkygkeeOQA"
     const response = await SummaryController.postSARAPI(req, res)
     expect(response.status).toBe(200)
     expect(response.text).toBe('')
