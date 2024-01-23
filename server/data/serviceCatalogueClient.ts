59609b33649fac066f2b9aac78ff77554c4bdef1
HorstmannA <anna.horstmann@madetech.com>
2024-01-23 08:37:47 +0000

    Revert service catalogue changes for now.

--- server/data/serviceCatalogueClient.ts
+++ server/data/serviceCatalogueClient.ts
@@ -13,7 +13,7 @@ export default class ServiceCatalogueClient {
     const token = await ServiceCatalogueClient.getUserToken()
     try {
       const response = await superagent
-        .get(`${config.apis.serviceCatalogue.url}`)
+        .get(`${config.apis.serviceCatalogue.url}/sar-report-components?env=dev`)
         .set('Authorization', `OAuth ${token}`)
       const { body } = response
       return body
