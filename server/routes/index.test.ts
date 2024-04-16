99b33f76720b3904a644f9bc26f4f8fbb5db6c12
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-04-16 10:51:10 +0100

    Add call to API endpoint to get reports info. (#41)
    * Add call to API endpoint to get reports info.
    
    * Add stub for GetReports.
    
    * Add stub to cypress config.
    
    * Mock report response.
    
    * Add auth for reports request.
    
    * Add required parameters.
    
    * Correct test path.
    
    * Add global currentPage.
    
    * Correct regex path.
    
    * Remove comments.
    
    * Add reminder to use user token rather than system token.
    
    * Put message in right place.

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -2,6 +2,7 @@ import type { Express } from 'express'
 import request from 'supertest'
 import { appWithAllRoutes } from './testutils/appSetup'
 import ServiceSelectionController from '../controllers/serviceSelectionController'
+import ReportsController from '../controllers/reportsController'
 
 let app: Express
 
@@ -20,6 +21,32 @@ beforeEach(() => {
       environments: [{ id: 47270, url: 'https://prisoner-search-indexer-dev.prison.service.justice.gov.uk' }],
     },
   ])
+  ReportsController.getSubjectAccessRequestList = jest.fn().mockReturnValue({
+    reports: [
+      {
+        uuid: 'ae6f396d-f1b1-460b-8d13-9a5f3e569c1a',
+        dateOfRequest: '2024-12-20',
+        sarCaseReference: 'example1',
+        subjectId: 'B1234AA',
+        status: 'Pending',
+      },
+      {
+        uuid: '1e130369-f3fb-46ab-8855-abd621d0b032',
+        dateOfRequest: '2023-01-19',
+        sarCaseReference: 'example2',
+        subjectId: 'C2345BB',
+        status: 'Completed',
+      },
+      {
+        uuid: '756689d0-4a0b-405c-bf0c-312f11f9f1b7',
+        dateOfRequest: '2022-16-18',
+        sarCaseReference: 'example3',
+        subjectId: 'D3456CC',
+        status: 'Completed',
+      },
+    ],
+    numberOfReports: 3,
+  })
 })
 
 afterEach(() => {
