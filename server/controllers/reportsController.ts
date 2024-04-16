180988f79b6f2808c20ea3323ba854335b80a7cb
HorstmannA <anna.horstmann@madetech.com>
2024-04-16 09:28:22 +0100

    Remove comments.

--- server/controllers/reportsController.ts
+++ server/controllers/reportsController.ts
@@ -16,31 +16,6 @@ export default class ReportsController {
     const numberOfReports = response.body.length
     const reports = response.body
 
-    // const numberOfReports = 500
-    // const reports = [
-    //   {
-    //     uuid: 'ae6f396d-f1b1-460b-8d13-9a5f3e569c1a',
-    //     dateOfRequest: '2024-12-01',
-    //     sarCaseReference: '1-casereference',
-    //     subjectId: 'A1234AA',
-    //     status: 'Pending',
-    //   },
-    //   {
-    //     uuid: '1e130369-f3fb-46ab-8855-abd621d0b032',
-    //     dateOfRequest: '2023-07-30',
-    //     sarCaseReference: '2-casereference',
-    //     subjectId: 'B2345BB',
-    //     status: 'Completed',
-    //   },
-    //   {
-    //     uuid: '756689d0-4a0b-405c-bf0c-312f11f9f1b7',
-    //     dateOfRequest: '2022-12-30',
-    //     sarCaseReference: '3-casereference',
-    //     subjectId: 'C3456CC',
-    //     status: 'Completed',
-    //   },
-    // ]
-
     return { reports, numberOfReports }
   }
 
