5339f2eb689cb3b016a9ca2525eefe789f1fdec4
April Dawson <aprilmdawson@hotmail.com>
2024-09-02 18:11:36 +0100

    Update report download to customise file name

---
+++ server/controllers/reportDownloadController.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express'
+import getReport from '../services/report'
+import formatDate from '../utils/dateHelpers'
+
+export default class ReportDownloadController {
+  static getReport(req: Request, res: Response, next: NextFunction) {
+    const fileId = req.query.id as string
+    const sarCaseReference = req.query.sarCaseReference as string
+    const subjectId = req.query.subjectId as string
+
+    if (!fileId || !subjectId || !sarCaseReference) {
+      throw new Error('Report ID, subject ID or SAR case reference number missing.')
+    }
+
+    const downloadDate = formatDate(new Date().toISOString(), 'short')
+
+    res.set(
+      'Content-Disposition',
+      `attachment;filename="subject-access-request-report-${downloadDate}-${sarCaseReference}-${subjectId}"`,
+    )
+
+    getReport(req, res, next, fileId)
+  }
+}
