4db20d62c2d2cc4ce77f73a4fdb1099b66454b50
thomasridd <twridd@gmail.com>
2024-10-14 14:55:37 +0100

    #274 Remove getUserRoles as an api call and add as decoded from the token
    https://github.com/ministryofjustice/hmpps-template-typescript/pull/274

--- server/utils/testUtils.ts
+++ server/utils/testUtils.ts
@@ -1,4 +1,18 @@
+import jwt from 'jsonwebtoken'
 import { AuditEvent } from '../audit'
 
 // eslint-disable-next-line import/prefer-default-export, @typescript-eslint/no-explicit-any
 export const auditAction = (action: AuditEvent): any => expect.objectContaining({ action })
+
+export function createUserToken(authorities: string[]) {
+  const payload = {
+    user_name: 'user1',
+    scope: ['read', 'write'],
+    auth_source: 'nomis',
+    authorities,
+    jti: 'a610a10-cca6-41db-985f-e87efb303aaf',
+    client_id: 'clientid',
+  }
+
+  return jwt.sign(payload, 'secret', { expiresIn: '1h' })
+}
