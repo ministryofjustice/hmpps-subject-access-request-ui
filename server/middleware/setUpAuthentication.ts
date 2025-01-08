8345d27020868956e8021f9a8f00bb62c6ca1125
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2025-01-08 16:04:14 +0000

    updated callback handler to logout error info to help debug issue

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -5,6 +5,7 @@ import flash from 'connect-flash'
 import config from '../config'
 import auth from '../authentication/auth'
 import { HmppsUser } from '../interfaces/hmppsUser'
+import logger from '../../logger'
 
 const router = express.Router()
 
@@ -22,12 +23,38 @@ export default function setUpAuth(): Router {
 
   router.get('/sign-in', passport.authenticate('oauth2'))
 
-  router.get('/sign-in/callback', (req, res, next) =>
-    passport.authenticate('oauth2', {
-      successReturnToOrRedirect: req.session.returnTo || '/',
-      failureRedirect: '/autherror',
-    })(req, res, next),
-  )
+  router.get('/sign-in/callback', (req, res, next) => {
+    // eslint-disable-next-line consistent-return
+    const authCallback: passport.AuthenticateCallback = (err, user, info) => {
+      if (err) {
+        logger.error('oauth2 /sign-in/callback returned error', JSON.stringify(err), err)
+        return res.redirect('/autherror')
+      }
+
+      if (!user) {
+        const { message } = info as { message: string }
+        if (info && message === 'Unable to verify authorization request state.') {
+          // failure to due authorisation state not being there on return, so retry
+          logger.info('Retrying auth callback as no state found')
+          return res.redirect('/')
+        }
+        logger.warn(`Auth failure due to ${JSON.stringify(info)}`)
+        return res.redirect('/autherror')
+      }
+
+      const { returnTo } = req.session
+      req.logIn(user, err2 => {
+        if (err2) {
+          return next(err2)
+        }
+        if (typeof returnTo === 'string' && returnTo.startsWith('/')) {
+          return res.redirect(returnTo)
+        }
+        return res.redirect('/')
+      })
+    }
+    passport.authenticate('oauth2', authCallback)(req, res, next)
+  })
 
   const authUrl = config.apis.hmppsAuth.externalUrl
   const authParameters = `client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
