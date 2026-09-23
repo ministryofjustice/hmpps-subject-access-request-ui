362665a2f6442bcffab91dc6e998d3823352477d
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2026-09-23 17:22:57 +0100

    update header to new version

--- integration_tests/pages/abstractPage.ts
+++ integration_tests/pages/abstractPage.ts
@@ -22,7 +22,6 @@ export default class AbstractPage {
     this.phaseBanner = page.getByTestId('header-phase-banner')
     this.usersName = page.getByTestId('header-user-name')
     this.signoutLink = page.getByText('Sign out')
-    this.manageUserDetails = page.getByTestId('manageDetails')
     this.header = page.locator('h1', { hasText: title })
   }
 
